8e17d0f6ceeef634bd48ed9739a5b28db9a820b9
hsbysddq <hsby15sddq@gmail.com>
2021-12-23 03:41:34 +0700

    resolve and complate edit funcion

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,176 @@
-import React, { useState } from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/home.page";
-import { Edit } from "./pages/edit.page";
-import { Player } from "./pages/player.page";
+import 'antd/dist/antd.css';
+import './App.css';
+import { Button, Table, Modal, Input } from 'antd';
+import { useState } from 'react';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
-const App = () => {
+function App() {
+  const [isEditing, setIsEditing] = useState(false)
+  const [editingPlayer, setEditingPlayer] = useState(null)
   const [players, setPlayers] = useState([
     {
       id: 1,
-      username: 'user',
-      email: 'user@gmail.com',
-      password: 'Katasandi1'
+      username: 'hasby',
+      email: 'hasby@gmail.com',
+      experience: '1',
+      level: '1'
     },
     {
       id: 2,
-      username: 'user2',
-      email: 'user2@gmail.com',
-      password: 'Katasandi12'
-    }
+      username: 'ubaddi',
+      email: 'ubaddi@gmail.com',
+      experience: '2',
+      level: '2'
+    },
+    {
+      id: 3,
+      username: 'fatah',
+      email: 'fatah@gmail.com',
+      experience: '3',
+      level: '3'
+    },
+    {
+      id: 4,
+      username: 'nashrullah',
+      email: 'nashrullah@gmail.com',
+      experience: '4',
+      level: '4'
+    },
   ])
-  const [tmpPlayer, setTmpPlayer] = useState({
-    username: '',
-    email: '',
-    password: ''
-  })
 
-  const onCreate = () => {
-    setPlayers([
-      ...players,
-      {
-        id: Date.now(),
-        username: tmpPlayer.username,
-        email: tmpPlayer.email,
-        password: tmpPlayer.password
+  const columns = [
+    {
+      key: '1',
+      title: 'ID',
+      dataIndex: 'id'
+    },
+    {
+      key: '2',
+      title: 'Username',
+      dataIndex: 'username'
+    },
+    {
+      key: '3',
+      title: 'Email',
+      dataIndex: 'email'
+    },
+    {
+      key: '4',
+      title: 'Experience',
+      dataIndex: 'experience'
+    },
+    {
+      key: '5',
+      title: 'Level',
+      dataIndex: 'level'
+    },
+    {
+      key: '6',
+      title: 'Actions',
+      render: (record) => {
+        return (
+        <>
+          <EditOutlined 
+            onClick={ () => {
+              onEditPlayer(record);
+            }}
+          />
+
+          <DeleteOutlined 
+            onClick={ () => {
+              onDeletePlayer(record)
+            }}
+            style={{ color: "red", marginLeft: 12 }} 
+          />
+        </>
+        );
+      },
+    },
+  ];
+
+  const onDeletePlayer = (record) => {
+    Modal.confirm({
+      title: 'Are you sure, you want to delete player',
+      okText: 'Yes',
+      okType: 'danger',
+      onOk:() => {
+        setPlayers( (pre) => {
+          return pre.filter(player => player.id != record.id);
+        });
       }
-    ])
-    setTmpPlayer({
-      username: '',
-      email: '',
-      password: ''
     })
+    
+  };
+
+  const onEditPlayer = (record) => {
+    setIsEditing(true);
+    setEditingPlayer({...record})
+  };
+
+  const resetEditing = () => {
+    setIsEditing(false);
+    setEditingPlayer(null)
   }
-
-  const onDelete = (id) => (e) => {
-    const newPlayers = [...players].filter(player => player.id !== id)
-    setPlayers(newPlayers)
-  }
-
-  const onEdit = (id) => {
-    setPlayers([
-      ...players,
-      {
-        username: tmpPlayer.username,
-        email: tmpPlayer.email
-      }
-    ])
-    setTmpPlayer({
-      username: '',
-      email: '',
-      password: ''
-    })
-  }
-
-
 
   return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home players={players} onDelete={onDelete} />} />
-          <Route path="/player" element={<Player onCreate={onCreate} setTmpPlayer={setTmpPlayer} tmpPlayer={tmpPlayer} />} />
-          <Route path="/edit" element={<Edit onEdit={onEdit} setTmpPlayer={setTmpPlayer} tmpPlayer={tmpPlayer} />} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
-  )
+    <div className="App">
+      <header className="App-header">
+        <Table columns={columns} dataSource={players}></Table>
+        <Modal
+          title= "Edit Player"
+          visible= {isEditing}
+          okText= "Save"
+          onCancel= { () => {
+            resetEditing()
+          }}
+          onOk= { () => {
+            setPlayers(pre => {
+              return pre.map(player => {
+                if (player.id === editingPlayer.id) {
+                  return editingPlayer;
+                } else {
+                  return player;
+                }
+              });
+            });
+            resetEditing();
+          }}
+        >
+          <Input value={editingPlayer?.username} 
+            onChange={ (e) => {
+              setEditingPlayer(pre => {
+                return { ...pre, username:e.target.value };
+              });
+            }}
+          />
+
+          <Input value={editingPlayer?.email} 
+            onChange={ (e) => {
+              setEditingPlayer(pre => {
+                return { ...pre, email:e.target.value };
+              });
+            }}
+          />
+
+          <Input value={editingPlayer?.experience} 
+            onChange={ (e) => {
+              setEditingPlayer(pre => {
+                return { ...pre, experience:e.target.value };
+              });
+            }}
+          />
+
+          <Input value={editingPlayer?.level} 
+            onChange={ (e) => {
+              setEditingPlayer(pre => {
+                return { ...pre, level:e.target.value };
+              });
+            }}
+          />
+        </Modal>
+      </header>
+    </div>
+  );
 }
 
 export default App;
